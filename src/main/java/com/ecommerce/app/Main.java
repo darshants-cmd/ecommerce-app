94010c420b21a3e40d6e13dfafba1b1ca454c4b5
darshants <darshants166@gmail.com>
2026-04-24 13:26:26 +0530

    Day1: Initial project setup

---
+++ src/main/java/com/ecommerce/app/Main.java
@@ -0,0 +1,11 @@
+package com.ecommerce.app;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("ECommerce App Started");
+
+	}
+
+}
